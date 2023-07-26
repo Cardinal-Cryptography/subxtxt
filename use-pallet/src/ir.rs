b02218b75287745d38a73c7ec822535af0db538e
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-07-26 14:24:15 +0200

    Intermediate representation

--- use-pallet/src/ir.rs
+++ use-pallet/src/ir.rs
@@ -1,9 +1,28 @@
 use proc_macro2::TokenStream as TokenStream2;
 
-pub struct PalletConfiguration {}
+pub struct PalletConfiguration {
+    pallet_name: syn::Ident,
+    type_declarations: Vec<syn::ItemType>,
+}
 
 impl PalletConfiguration {
     pub fn new(_attr: TokenStream2, input: TokenStream2) -> Result<Self, syn::Error> {
-        Ok(Self {})
+        let item_mod = syn::parse2::<syn::ItemMod>(input)?;
+
+        let type_declarations = item_mod
+            .content
+            .unwrap_or_default()
+            .1
+            .iter()
+            .filter_map(|item| match item {
+                syn::Item::Type(item_type) => Some(item_type.clone()),
+                _ => None,
+            })
+            .collect();
+
+        Ok(Self {
+            pallet_name: item_mod.ident,
+            type_declarations,
+        })
     }
 }
